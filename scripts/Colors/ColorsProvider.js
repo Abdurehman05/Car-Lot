dee90a69f12334657698384fe44746abd40ae189
Abdurehman05 <abdukr5@gmail.com>
2020-09-17 14:33:36 -0500

    array of cars and colors fetched in their respectve module

--- scripts/Colors/ColorsProvider.js
+++ scripts/Colors/ColorsProvider.js
@@ -1,13 +1,19 @@
 // This is the fetch URL to get an array of available colors
 const availableColorsURL =
-  "https://gist.githubusercontent.com/AdamSheaffer/fe50daf69c713eeb1a83edc2525a4643/raw/87785ea0ac82007ae1cde7370ffb3b97054b1c41/cars.json";
+    "https://gist.githubusercontent.com/AdamSheaffer/fe50daf69c713eeb1a83edc2525a4643/raw/87785ea0ac82007ae1cde7370ffb3b97054b1c41/cars.json";
 
 let availableColors = [];
 
 export const useColors = () => {
-  // TODO
+
+    return availableColors.slice()
+
 };
 
 export const getAvailableColors = () => {
-  // TODO
+    return fetch(availableColorsURL)
+        .then(res => res.json())
+        .then(parsedRes => {
+            availableColors = parsedRes
+        })
 };
